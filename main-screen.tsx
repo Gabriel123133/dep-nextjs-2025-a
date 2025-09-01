5b1caf15f43717df1a2eb95ba0d99da5c121010d
Gabriel123133 <gabriel1827111@gmail.com>
2025-09-01 11:18:22 +0000

    Aula01/09/2025

---
+++ main-screen.tsx
@@ -0,0 +1,9 @@
+const MainScreen = () =>{
+    return(
+        <main>
+            <h1>Exemplo de Aplicação Next</h1>
+            <p>Teste de IMplantação</p>
+        </main>
+    );
+}
+export default MainScreen;
